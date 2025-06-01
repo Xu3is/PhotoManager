435232bc1dacd179bc8d54462a4dff905e2045e1
Xu3is <mleliukov@yandex.ru>
2025-06-02 02:57:55 +0300

    Добавлен основной класс Photo

---
+++ PhotoManager/PhotoManager/Photo.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System;
+
+namespace PhotoManager
+{
+    public class Photo
+    {
+        public string Path { get; set; }
+        public string Description { get; set; }
+        public DateTime DateTaken { get; set; }
+
+        public Photo(string path, string description, DateTime dateTaken)
+        {
+            Path = path;
+            Description = description;
+            DateTaken = dateTaken;
+        }
+
+        public override string ToString()
+        {
+            return $"{Path} - {Description} ({DateTaken.ToString("dd.MM.yyyy")})";
+        }
+    }
+}
