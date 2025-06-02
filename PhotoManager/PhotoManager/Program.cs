9611da1a7a0e27f3f3a08b224c9a50549ff269f5
Xu3is <mleliukov@yandex.ru>
2025-06-02 03:04:36 +0300

    Добавлена основная форма управления фотографиями PhotoAlbumForm

--- PhotoManager/PhotoManager/Program.cs
+++ PhotoManager/PhotoManager/Program.cs
@@ -16,7 +16,7 @@ namespace PhotoManager
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new PhotoAlbumForm());
         }
     }
 }
